e131331f7230843778f9728d9d11b6e96e206f48
dniel <daniel@engfeldt.net>
2020-04-25 12:08:10 +0200

    Add test for authenticated user

--- src/whoami.test.js
+++ src/whoami.test.js
@@ -1,12 +1,14 @@
 import http from "k6/http";
-import {check, group, sleep} from "k6";
-import {baseFqdn, options, thinktime2} from "./common.js";
+import encoding from "k6/encoding";
+import {check, group} from "k6";
+import {baseFqdn, clientId, clientSecret, options} from "./common.js";
 
 // Export options object so k6 can access it
 export {options};
 
 /**
- * Test that the whoami service is protected.
+ * Test that the whoami service is protected
+ * without credentials.
  * TODO: add check for correct redirect url.
  */
 function whoamiUnauthenticated() {
@@ -16,12 +18,23 @@ function whoamiUnauthenticated() {
         "status is 307": (r) => r.status === 307,
     });
 }
+
 /**
- * Test that the whoami service is protected.
+ * Test that the whoami service is available
+ * with credentials
  */
 function whoamiAuthenticated() {
     const url = `https://whoami.${baseFqdn}`
-    let res = http.get(url, {follow: false, redirects: 0});
+    const options = {
+        headers: {
+            "Authorization": "Basic " + encoding.b64encode(`${clientId}:${clientSecret}`)
+        },
+        follow: false, redirects: 0
+    };
+
+    // test url call.
+    let res = http.get(url, options);
+
     check(res, {
         "status is 200": (r) => r.status === 200,
     });
