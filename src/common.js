df1bec1b234e93dd908877dc0e4617df83d4f203
dniel <daniel@engfeldt.net>
2020-04-26 21:39:27 +0200

    remove duration.

--- src/common.js
+++ src/common.js
@@ -3,11 +3,10 @@ import {Trend} from "k6/metrics";
 // Default runtime options
 export let options = {
     vus: 1,
-    duration: "5s",
     thresholds: {
         errors: ["rate<0.1"], // <10% errors
-        transaction_time: ["avg<1000"], // Require transaction_time's average to be <1000ms
-        http_req_duration: ["avg<2000"], // Require http_req_duration's average to be <2000ms
+        transaction_time: ["avg<500"], // Require transaction_time's average to be <1000ms
+        http_req_duration: ["avg<1000"], // Require http_req_duration's average to be <2000ms
     }
 };
 
