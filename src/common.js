5487df9a36ac72714e1c3dba93097d110117e207
dniel <daniel@engfeldt.net>
2020-05-03 12:27:48 +0200

    change from let to const for constants

--- src/common.js
+++ src/common.js
@@ -1,7 +1,7 @@
 import {Counter, Trend} from "k6/metrics";
 
 // Default runtime options
-export let options = {
+export const options = {
     vus: 1,
     thresholds: {
         checks: ["rate == 1.0"], // all checks should pass.
@@ -12,18 +12,18 @@ export let options = {
 
 // Create a Trend metric to hold transaction time data samples from the HTTP calls to the various end points
 // Please see note below about this metric and the thresholds set in 'options' above
-export let myTrend = new Trend("transaction_time");
+export const myTrend = new Trend("transaction_time");
 
 // baseHostname that we prepend to all URLs we use
-export let baseFqdn = `${__ENV.BASE_FQDN}`;
+export const baseFqdn = `${__ENV.BASE_FQDN}`;
 
 // credentials
-export let clientId = `${__ENV.CLIENT_ID}`;
-export let clientSecret = `${__ENV.CLIENT_SECRET}`;
+export const clientId = `${__ENV.CLIENT_ID}`;
+export const clientSecret = `${__ENV.CLIENT_SECRET}`;
 
 // Think times, to slow down execution somewhat
-export let thinktime1 = 0.1;
-export let thinktime2 = 2.0;
+export const thinktime1 = 0.1;
+export const thinktime2 = 2.0;
 
 const stringConstructor = "test".constructor;
 const arrayConstructor = [].constructor;
